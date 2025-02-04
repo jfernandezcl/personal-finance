beb4030a7a9cca80df682b197c47a3a36242ff36
jfernandecl <javierfernandez0987@gmail.com>
2025-02-04 12:49:08 +0100

    feat: create html and styles user registration

--- frontend/src/components/SignUpPage.jsx
+++ frontend/src/components/SignUpPage.jsx
@@ -1,4 +1,8 @@
 import logo from '../images/logo.svg'
+import iconEmail from '../images/icon-email.svg'
+import iconPassword from '../images/icon-password.svg'
+import iconGoogle from '../images/icon-google.svg'
+import iconUser from '../images/icon-user.svg'
 
 function SignUpPage() {
 
@@ -18,6 +22,83 @@ function SignUpPage() {
               <span className="mt-20 inline-block">
                 <img className="w-[350px] h-[350px]" src="src/images/mobile-illustration.svg" alt="illustration" />
               </span>
+            </div>
+          </div>
+
+          {/*Registro de usuarios*/}
+          <div className="w-full border-stroke xl:w-1/2 xl:border-l-2 px-[70px]">
+            <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
+              <span className="mb-1.5 block font-medium text-[#788596]">
+                Start for free
+              </span>
+              <h2 className="mb-9 text-2xl font-bold text-black sm:text-title-xl2">
+                Sign Up to Personal finances
+              </h2>
+              <form>
+                <div className="mb-4">
+                  <label className="mb-2.5 block font-medium text-black">Name</label>
+                  <div className="relative">
+                    <input type="text" placeholder="Enter your full name" className="w-full rounded-lg border 
+                              border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary 
+                              focus-visible:shadow-none"/>
+                    <span className="absolute right-4 top-4">
+                      <img className="fill-current" width="22" height="22" src={iconUser} />
+                    </span>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label className="mb-2.5 block font-medium text-black">Email</label>
+                  <div className="relative">
+                    <input type="email" placeholder="Enter your email" className="w-full rounded-lg border 
+                              border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary 
+                              focus-visible:shadow-none"/>
+                    <span className="absolute right-4 top-4">
+                      <img className="fill-current" width="22" height="22" src={iconEmail} />
+                    </span>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label className="mb-2.5 block font-medium text-black">Password</label>
+                  <div className="relative">
+                    <input type="password" placeholder="Enter your password" className="w-full rounded-lg border 
+                              border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary 
+                              focus-visible:shadow-none" />
+                    <span className="absolute right-4 top-4">
+                      <img className="fill-current" width="22" height="22" src={iconPassword} />
+                    </span>
+                  </div>
+                </div>
+                <div className="mb-6">
+                  <label className="mb-2.5 block font-medium text-black">Re-type Password</label>
+                  <div className="relative">
+                    <input type="password" placeholder="Re-enter your password" className="w-full rounded-lg border 
+                              border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary 
+                              focus-visible:shadow-none" />
+                    <span className="absolute right-4 top-4">
+                      <img className="fill-current" width="22" height="22" src={iconPassword} />
+                    </span>
+                  </div>
+                </div>
+                <div className="mb-5">
+                  <input type="submit" value="Create account" className="w-full cursor-pointer rounded-lg 
+                              border border-[#3c50e0] bg-[#3c50e0] p-4 font-medium text-white transition 
+                              hover:bg-opacity-90"/>
+                </div>
+                <button className="flex w-full items-center justify-center gap-3.5 rounded-lg 
+                            border border-[#D1D5DB] bg-[#E2E8F0] p-4 font-medium text-[#788596] 
+                            hover:bg-opacity-70">
+                  <span>
+                    <img className="fill-current" width="20" height="20" src={iconGoogle} />
+                  </span>
+                  Sign in with Google
+                </button>
+                <div className="mt-6 text-center">
+                  <p className="font-medium text-[#788596]">
+                    Already have an account?
+                    <a href="#"> Sign in</a>
+                  </p>
+                </div>
+              </form>
             </div>
           </div>
         </div>
