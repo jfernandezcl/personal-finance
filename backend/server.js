a2eb2fde11988488eeed66ae420615cd51879be5
jfernandecl <javierfernandez0987@gmail.com>
2025-02-18 12:49:11 +0100

    feat: Implement protected paths and correct format of userId to hexadecimal in the profile

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRouter);
 
 const PORT = process.env.PORT || 5000;
 
