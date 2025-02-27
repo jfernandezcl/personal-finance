47d899851381efe301b37dd706c5f4e69cc0d5a7
jfernandecl <javierfernandez0987@gmail.com>
2025-02-27 11:54:20 +0100

    fix: update colours of everything in general

--- frontend/src/components/BalanceCard.jsx
+++ frontend/src/components/BalanceCard.jsx
@@ -2,24 +2,29 @@ import React from "react";
 
 export default function BalanceCard() {
   return (
-    <div className="bg-green-700 text-white p-6 max-w-6xl mx-auto rounded-lg shadow mb-8">
+    <div className="bg-[#025963] text-white p-6 max-w-6xl mx-auto rounded-lg shadow mb-8">
       <div className="flex items-center justify-between">
         <div>
-          <h1 className="text-2xl font-bold">€320,845.20</h1>
-          <p className="text-sm text-green-200">15.9% ↑</p>
+          Total Balance
+          <div className="flex items-baseline space-x-2">
+            <h1 className="text-2xl font-bold">320,845.20 €</h1>
+            <p className="text-sm text-green-200">15.9% ↑</p>
+          </div>
         </div>
         <div className="space-x-3">
-          <button className="bg-green-800 hover:bg-green-900 transition px-4 py-2 rounded-md">
+          <button className="bg-[#00d57d] hover:bg-[#1f5b63] transition px-4 py-2 rounded-md">
             + Add
           </button>
-          <button className="bg-green-800 hover:bg-green-900 transition px-4 py-2 rounded-md">
+          <button className="bg-[#29737e] hover:bg-[#1f5b63] transition px-4 py-2 rounded-md">
             Send
           </button>
-          <button className="bg-green-800 hover:bg-green-900 transition px-4 py-2 rounded-md">
+          <button className="bg-[#29737e] hover:bg-[#1f5b63] transition px-4 py-2 rounded-md">
             Request
           </button>
         </div>
       </div>
     </div>
+
+
   );
 }
--- frontend/src/components/IncomeExpenseCards.jsx
+++ frontend/src/components/IncomeExpenseCards.jsx
@@ -4,13 +4,13 @@ export default function IncomeExpenseCards() {
   return (
     <div className="flex flex-col gap-6">
       <div className="bg-white rounded-lg shadow p-6">
-        <p className="text-sm font-semibold text-gray-400">Income</p>
-        <h3 className="text-xl font-bold text-gray-800">€12,378.20</h3>
+        <p className="text-sm font-semibold text-black">Income</p>
+        <h3 className="text-xl font-bold text-black">12,378.20 €</h3>
         <span className="text-sm text-green-500 font-semibold">+450%</span>
       </div>
       <div className="bg-white rounded-lg shadow p-6">
-        <p className="text-sm font-semibold text-gray-400">Expense</p>
-        <h3 className="text-xl font-bold text-gray-800">€5,788.21</h3>
+        <p className="text-sm font-semibold text-black">Expense</p>
+        <h3 className="text-xl font-bold text-black">5,788.21 €</h3>
         <span className="text-sm text-red-500 font-semibold">-10%</span>
       </div>
     </div>
