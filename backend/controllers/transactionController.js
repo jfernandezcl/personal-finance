2a141d09266b800c582738689fee6cf2afe339ed
jfernandecl <javierfernandez0987@gmail.com>
2025-03-13 20:10:08 +0100

    feat: create the routes and controllers of the transactions

---
+++ backend/controllers/transactionController.js
@@ -0,0 +1,40 @@
+import pool from '../database/db.js';
+
+//Agregar una nueva transacción
+export const addTransaction = async (req, res) => {
+  const { type, amount, description } = req.body;
+  const user_id = req.userId; // Lo obtenemos del middleware de autenticación
+
+  if (!type || !amount || !description) {
+    return res.status(400).json({ msg: "All fields are required" });
+  }
+
+  try {
+    await pool.execute(
+      "INSERT INTO transactions (user_id, type, amount, description) VALUES (?, ?, ?, ?)",
+      [user_id, type, amount, description]
+    );
+
+    res.status(201).json({ msg: "Transaction added successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+// Obtener transacciones del usuario autenticado
+export const getTransactions = async (req, res) => {
+  const user_id = req.userId;
+
+  try {
+    const [transactions] = await pool.execute(
+      "SELECT BIN_TO_UUID(id) as id, type, amount, description, date FROM transactions WHERE user_id = ?",
+      [user_id]
+    );
+
+    res.json(transactions);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
