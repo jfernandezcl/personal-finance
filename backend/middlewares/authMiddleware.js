ddb3922ebb58cedf1766ff7001b3c3b9cfdcaa65
jfernandecl <javierfernandez0987@gmail.com>
2025-04-03 15:42:44 +0200

    feat: add new UUID type definitions and related files

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,22 +1,22 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-import { stringify } from 'uuid';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { stringify } from "uuid";
 
 dotenv.config();
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
+  const token = req.headers["authorization"]?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({
-      message: 'Unauthorized'
+      message: "Unauthorized",
     });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({
-        message: 'Forbidden'
+        message: "Forbidden",
       });
     }
 
