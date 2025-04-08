87a49d1f0fa2de26ddb5fc231647376b1363e357
jfernandecl <javierfernandez0987@gmail.com>
2025-04-08 11:28:25 +0200

    feat: add verify route to authRoutes for token verification

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import { register, login } from '../controllers/authController.js';
+import express from "express";
+import { register, login, verifyToken } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post('/register', register);  // Usando la función register del controlador
-router.post('/login', login);        // Usando la función login del controlador
+router.post("/register", register); // Usando la función register del controlador
+router.post("/login", login); // Usando la función login del controlador
+router.get("/verify", verifyToken); // Usando la función verifyToken del controlador
 
 export default router;
