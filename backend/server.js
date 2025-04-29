177fc0f6cba63959a8736160192b354d07b6e354
jfernandecl <javierfernandez0987@gmail.com>
2025-04-29 12:43:47 +0200

    feat: enhance username update functionality and improve token generation

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,22 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import transactionRoutes from "./routes/transactionRoutes.js";
 
 import corsMiddleware from "./middleware/cors.js";
-import loginWithGoogle from "./controllers/googleAuthController.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(corsMiddleware);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/auth", loginWithGoogle);
 
 // Agregar las rutas de transacciones
 app.use("/api/transactions/", transactionRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
