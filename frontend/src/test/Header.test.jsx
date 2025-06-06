3e728279474fbc9e60e466abfbc030c3fe47eb9a
jfernandecl <javierfernandez0987@gmail.com>
2025-06-06 17:15:53 +0200

    test: extract renderHeader function to improve readability of tests and localStorage test

--- frontend/src/test/Header.test.jsx
+++ frontend/src/test/Header.test.jsx
@@ -3,11 +3,22 @@ import { render, screen } from "@testing-library/react";
 import Header from "../components/Header";
 import { BrowserRouter } from "react-router-dom";
 
-test("muestra el texto 'Personal finance' en el header", () => {
+const renderHeader = () => {
   render(
     <BrowserRouter>
       <Header />
     </BrowserRouter>
   );
+};
+
+test("displays the text 'Personal finance' in the header", () => {
+  renderHeader();
   expect(screen.getByText("Personal Finance")).toBeInTheDocument();
+});
+
+test("displays the user's initials in the header", () => {
+  localStorage.setItem("username", "John Doe");
+  renderHeader();
+
+  expect(screen.getByText("JD")).toBeInTheDocument();
 });
