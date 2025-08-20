bca6ca609035f10e5cb074463fc2adedb12b7599
jfernandecl <javierfernandez0987@gmail.com>
2025-08-20 12:57:04 +0200

    feature: i18n on the user registration page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -11,6 +11,8 @@ import { GoogleLogin } from "@react-oauth/google";
 import hideEye from "../images/hide-eye.svg";
 import showEye from "../images/show-eye.svg";
 
+import { useTranslation } from "react-i18next";
+
 function SignUpPage() {
   const navigate = useNavigate();
   const [error, setError] = useState("");
@@ -25,13 +27,15 @@ function SignUpPage() {
 
   const [showPassword, setShowPassword] = useState(false);
 
+  const { t, i18n } = useTranslation();
+
   const handleChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (formData.password !== formData.confirmPassword) {
-      setError("Passwords do not match");
+      setError(t("messages.errors.password_error"));
       setTimeout(() => setError(""), 3000);
       return;
     }
@@ -48,17 +52,17 @@ function SignUpPage() {
       });
       const data = await response.json();
       if (!response.ok) {
-        setError(data.message || "The user already exists, enter other data");
+        setError(data.message || t("messages.errors.data_message"));
         setTimeout(() => setError(""), 3000);
         return;
       }
-      setSuccessMessage("Registration successful!");
+      setSuccessMessage(t("success.registration_successful"));
       setTimeout(() => {
         setSuccessMessage("");
         navigate("/dashboard");
       }, 2000);
     } catch {
-      setError("Server error");
+      setError(t("messages.errors.server_error"));
       setTimeout(() => setError(""), 3000);
     }
   };
@@ -74,20 +78,20 @@ function SignUpPage() {
       const data = await res.json();
 
       if (!res.ok) {
-        setError("Google login failed. Please try again.");
+        setError(t("messages.errors.google_failed"));
         setTimeout(() => setError(""), 3000);
         return;
       }
       localStorage.setItem("token", data.token);
       localStorage.setItem("username", data.username);
 
-      setSuccess("Login successful!");
+      setSuccess(t("messages.success.login_successful"));
       setTimeout(() => {
         setSuccess("");
         navigate("/dashboard");
       }, 1500);
     } catch {
-      setError("Server error during Google login. Please try again later.");
+      setError(t("messages.errors.error_google_login"));
       setTimeout(() => setError(""), 3000);
     }
   };
@@ -105,15 +109,17 @@ function SignUpPage() {
         />
       )}
       <div className="rounded-sm bg-white shadow-default">
-        <div className="flex flex-wrap items-center mt-10 pb-10">
+        <div className="flex flex-wrap items-center mt-4 pb-10">
           <div className="hidden w-full xl:block xl:w-1/2">
             <div className="px-26 py-17.5 text-center">
               <a className="mb-5.5 flex items-center justify-center gap-2">
                 <img className="w-10 h-auto" src={logo} alt="logo" />
-                <span className="text-lg font-semibold">Personal finances</span>
+                <span className="text-lg font-semibold">
+                  {t("common.appName")}
+                </span>
               </a>
               <p className="font-medium 2xl:px-20 mt-6">
-                Manage your personal finances easily and efficiently.
+                {t("common.information")}
               </p>
               <span className="mt-20 inline-block">
                 <img
@@ -125,23 +131,37 @@ function SignUpPage() {
             </div>
           </div>
           <div className="w-full border-stroke xl:w-1/2 xl:border-l-2 px-[70px]">
+            <div className="flex justify-end">
+              <button
+                className="px-4 py-1 m-2 cursor-pointer rounded-lg border border-[#025963] bg-[#025963] p- font-medium text-white transition hover:bg-opacity-90"
+                onClick={() => i18n.changeLanguage("en")}
+              >
+                EN
+              </button>
+              <button
+                className="px-4 py-1 m-2 cursor-pointer rounded-lg border border-[#025963] bg-[#025963] p- font-medium text-white transition hover:bg-opacity-90"
+                onClick={() => i18n.changeLanguage("es")}
+              >
+                ES
+              </button>
+            </div>
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
               <span className="mb-1.5 block font-medium text-[#788596]">
-                Start for free
+                {t("common.start")}
               </span>
               <h2 className="mb-9 text-2xl font-bold text-black sm:text-title-xl2">
-                Sign Up to Personal finances
+                {t("signUpPage.signupTitle")}
               </h2>
               <form onSubmit={handleSubmit}>
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black">
-                    Name
+                    {t("common.name.label")}
                   </label>
                   <div className="relative">
                     <input
                       type="text"
                       name="username"
-                      placeholder="Enter your full name"
+                      placeholder={t("common.name.placeholder")}
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none"
                       onChange={handleChange}
                       value={formData.username}
@@ -159,13 +179,13 @@ function SignUpPage() {
                 </div>
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black">
-                    Email
+                    {t("common.email.label")}
                   </label>
                   <div className="relative">
                     <input
                       type="email"
                       name="email"
-                      placeholder="Enter your email"
+                      placeholder={t("common.email.placeholder")}
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none"
                       onChange={handleChange}
                       value={formData.email}
@@ -183,13 +203,13 @@ function SignUpPage() {
                 </div>
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black">
-                    Password
+                    {t("common.password.label")}
                   </label>
                   <div className="relative">
                     <input
                       type={showPassword ? "text" : "password"}
                       name="password"
-                      placeholder="Enter your password"
+                      placeholder={t("common.password.placeholder")}
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none"
                       onChange={handleChange}
                       value={formData.password}
@@ -209,13 +229,13 @@ function SignUpPage() {
                 </div>
                 <div className="mb-6">
                   <label className="mb-2.5 block font-medium text-black">
-                    Re-type Password
+                    {t("signUpPage.repeatPassword.label")}
                   </label>
                   <div className="relative">
                     <input
                       type={showPassword ? "text" : "password"}
                       name="confirmPassword"
-                      placeholder="Re-enter your password"
+                      placeholder={t("signUpPage.repeatPassword.placeholder")}
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none"
                       onChange={handleChange}
                       value={formData.confirmPassword}
@@ -238,7 +258,7 @@ function SignUpPage() {
                     type="submit"
                     className="w-full cursor-pointer rounded-lg border border-[#025963] bg-[#025963] p-4 font-medium text-white transition hover:bg-opacity-90"
                   >
-                    Create account
+                    {t("signUpPage.create")}
                   </button>
                 </div>
 
@@ -266,20 +286,19 @@ function SignUpPage() {
                             alt="google icon"
                           />
                         </span>
-                        Sign in with Google
                       </button>
                     )}
                   />
                 </div>
                 <div className="mt-6 text-center">
                   <p className="font-medium text-[#788596]">
-                    Already have an account?
+                    {t("signUpPage.login")}
                     <a
                       href="/"
                       className="text-[#025963] underline cursor-pointer"
                     >
                       {" "}
-                      Sign in
+                      {t("signUpPage.signin")}
                     </a>
                   </p>
                 </div>
