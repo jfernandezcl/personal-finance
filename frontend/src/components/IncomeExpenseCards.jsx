da8ad5556e89de45dcbd620eb4d83502fa788140
jfernandecl <javierfernandez0987@gmail.com>
2025-08-21 16:57:04 +0200

    feature: add i18n support for Income and Expense cards in English and Spanish

--- frontend/src/components/IncomeExpenseCards.jsx
+++ frontend/src/components/IncomeExpenseCards.jsx
@@ -1,8 +1,10 @@
 import { useDashboardContext } from "../context/DashboardContext";
 import illustrationcards from "../images/illustration-cards.svg";
+import { useTranslation } from "react-i18next";
 
 export default function IncomeExpenseCards() {
   const { transactions } = useDashboardContext();
+  const { t, i18n } = useTranslation();
 
   const income = transactions
     .filter((transaction) => transaction.type === "income")
@@ -15,7 +17,9 @@ export default function IncomeExpenseCards() {
   return (
     <div className="flex flex-col gap-6">
       <div className="relative bg-white rounded-lg shadow p-6">
-        <p className="text-sm font-semibold text-[#025963f8]">Income</p>
+        <p className="text-sm font-semibold text-[#025963f8]">
+          {t("dashboardPage.cardIE.income")}
+        </p>
         <h3 className="text-xl font-bold text-black">{income.toFixed(2)} €</h3>
         <img
           src={illustrationcards}
@@ -25,7 +29,9 @@ export default function IncomeExpenseCards() {
       </div>
 
       <div className="relative bg-white rounded-lg shadow p-6">
-        <p className="text-sm font-semibold text-[#00d57d]">Expense</p>
+        <p className="text-sm font-semibold text-[#00d57d]">
+          {t("dashboardPage.cardIE.expense")}
+        </p>
         <h3 className="text-xl font-bold text-black">{expense.toFixed(2)} €</h3>
         <img
           src={illustrationcards}
